664adbd483d96e92a4ce0fab8c2cc956ed499568
AlexisRoj <alexisroj2@gmail.com>
2016-09-16 20:33:44 -0600

    Se le agrega la funcionalidad del setOnItemClickListener y se mejora aspecto visual

---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/customlistviewadapter/spinner.java
@@ -0,0 +1,34 @@
+package com.innovagenesis.aplicaciones.android.customlistviewadapter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class spinner extends Fragment {
+
+    public spinner() {
+        // Required empty public constructor
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        /** Spinner Simple cuando un arrayAdapter se llama desde el main se usa this,
+         * pero si se llama desde un fragment se usa el getActivity**/
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
+                (getActivity(), R.array.horario_de_clases, android.R.layout.simple_spinner_item);
+
+        return inflater.inflate(R.layout.fragment_spinner2, container, false);
+    }
+
+}
