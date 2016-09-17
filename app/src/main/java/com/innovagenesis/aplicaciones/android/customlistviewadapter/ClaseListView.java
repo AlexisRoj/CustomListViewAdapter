35bc6eba43d27fd195a17a25fc49730fc76bdd39
AlexisRoj <alexisroj2@gmail.com>
2016-09-16 22:41:45 -0600

    Se incluye view

--- app/src/main/java/com/innovagenesis/aplicaciones/android/customlistviewadapter/ClaseListView.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/customlistviewadapter/ClaseListView.java
@@ -1,71 +1,63 @@
 package com.innovagenesis.aplicaciones.android.customlistviewadapter;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.innovagenesis.aplicaciones.android.customlistviewadapter.adapter.DiaHoraioAdapter;
 import com.innovagenesis.aplicaciones.android.customlistviewadapter.modelo.DiaHorario;
 
 import java.util.ArrayList;
+public class ClaseListView extends Fragment {
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
-public class ListView extends Fragment {
-
-
-    public ListView() {
+    public ClaseListView() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View view =  inflater.inflate(R.layout.fragment_list_view, container, false);
+
+        /***/
 
 
-        String[] titulos = getActivity().getResources().getStringArray(R.array.horario_de_clases); //Realiza la captura de los titulos
 
+        final String[] titulos = getActivity().getResources().getStringArray(R.array.horario_de_clases); //Realiza la captura de los titulos
         String[] subtitulos = getActivity().getResources().getStringArray(R.array.dias_semana); //Realiza la captura de los subtitulos
 
-        /** Se crea la lista que va a contener los datos **/
-        ArrayList<DiaHorario> lista = new ArrayList<>();
+        ArrayList<DiaHorario> lista = new ArrayList<>(); /* Se crea la lista que va a contener los datos */
 
         /** Se recorre con el largo de cualquiera de los dos Array **/
-
         for (int i = 0; i < titulos.length; i++) {
-
             /** Se llena usando la Clase de Captura**/
             lista.add(new DiaHorario(titulos[i], subtitulos[i]));
         }
-
         DiaHoraioAdapter adapter = new DiaHoraioAdapter(getActivity(), lista);
-
         /** Se enlaza el listView y se crea el adapter*/
 
 
-        ListView horario = (ListView) findViewById(R.id.listView);
+        ListView horario = (ListView) view.findViewById(R.id.listView);
         horario.setAdapter(adapter);
 
         horario.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                TextView opcion = (TextView) findViewById(R.id.opcion);
+                TextView opcion = (TextView) view.findViewById(R.id.opcion);
                 opcion.setText("Opcion" + titulos[i]); // Se le pasa el nombre que queramos
 
             }
         });
 
-        return inflater.inflate(R.layout.fragment_list_view, container, false);
+
+        return view;
     }
 
 }
